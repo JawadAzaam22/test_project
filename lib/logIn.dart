dd829b8c6c2580b1ec81dc45754f3ce157896339
jawad@example <jawad@example>
2024-09-16 17:53:52 +0300

    first time

---
+++ lib/logIn.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class loginscreen extends StatelessWidget {
+  const loginscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body:Center(
+          child: Text(
+            "ss"
+          ),
+      ) ,
+
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'logIn.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const loginscreen()
+    );
+  }
+}
